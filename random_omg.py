5f17d7cfba9f12e057c37407d211a54be19d79d8
Eli Antony <eliantony@live.com>
2016-01-22 19:30:26 -0800

    finished random OMG!

--- random_omg.py
+++ random_omg.py
@@ -1,13 +1,23 @@
 from sense_hat import SenseHat
 import time
+import random 
 
 sense = SenseHat()
+
 sense.set_rotation(180)
-sense.show_letter("O",text_colour = [255, 0, 0])
+r = random.randint (0, 255)
+sense.show_letter("O",text_colour = [0, r, r])
 time.sleep(2)
-sense.show_letter("M",text_colour = [0, 255, 0])
+
+r = random.randint(0, 255)
+sense.show_letter("M",text_colour = [r, r, 0])
 time.sleep(2)
-sense.show_letter("G",text_colour = [0, 0, 255])
+
+r = random.randint(0, 255)
+sense.show_letter("G",text_colour = [r, r, 0])
 time.sleep(2)
-sense.show_letter("!",text_colour = [255, 255, 255])
+
+r = random.randint(0, 255)
+sense.show_letter("!",text_colour = [0, r, r])
+time.sleep(1)
 sense.clear()
