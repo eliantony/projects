911142efe0056829594a66904cd59328edad80d2
Eli Antony <eliantony@live.com>
2016-01-27 20:32:19 -0800

    rainbow

---
+++ joke.py
@@ -0,0 +1,14 @@
+import time
+from sense_hat import SenseHat
+sense = SenseHat()
+
+sense.set_rotation(180)
+speed = 0.05
+sense.show_message("Knock Knock", scroll_speed = speed)
+time.sleep(2)
+sense.show_message("Kertich!" , scroll_speed = speed)
+time.sleep(2)
+sense.show_message("God Bless you!!" , scroll_speed = speed)
+
+time.sleep(2)
+sense.clear()
